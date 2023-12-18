91a05099cd3583d05e1dd424991b56f7937a2f25
Joana <joana.rparreira1994@gmail.com>
2023-12-18 22:52:48 +0000

    added Menu start, and CSS styling and Java logic

--- MyAPIQiuzz/view.js
+++ MyAPIQiuzz/view.js
@@ -4,11 +4,17 @@ define(function () {
 
   let indexRandom = 0;
   let questionNumber = 1;
-  
+
   internals.createQuestion = function (question) {
-    let questionAsked = question.results[indexRandom].question;
-    let questiontoAnswer = `<p>${questionAsked}</p>`
-    return questiontoAnswer;
+    if (
+      question.results &&
+      question.results[indexRandom] &&
+      question.results[indexRandom].question
+    ) {
+      let questionAsked = question.results[indexRandom].question;
+      let questiontoAnswer = `<p>${questionAsked}</p>`;
+      return questiontoAnswer;
+    }
   };
 
   internals.shuffleArray = function (array) {
@@ -18,115 +24,172 @@ define(function () {
     }
   };
 
+  internals.renderQuizz = function (question) {
+    var quizzCard = $("#app");
+
+    let questionCreated = internals.createQuestion(question);
+    $("#question").empty().append(questionCreated);
+    $("#questionNumber")
+      .empty()
+      .append(`Question: ` + questionNumber + `/10`);
+
+    internals.renderAnswers(question);
+    //internals.incrementIndex();
+    internals.incrementQuestion();
+    internals.createCategory(question);
+
+    console.log(question);
+  };
+
+
+
   internals.createAnswers = function (answer) {
-    var answers = [answer.results[indexRandom].correct_answer,
-                   answer.results[indexRandom].incorrect_answers[0],
-                   answer.results[indexRandom].incorrect_answers[1],
-                   answer.results[indexRandom].incorrect_answers[2]];
+
+    var arrayIncorrect = answer.results[indexRandom].incorrect_answers
+    
+    var correctAnswers = [answer.results[indexRandom].correct_answer];
+
+    var answers = correctAnswers.concat(arrayIncorrect);
+
     console.log(answers);
+
+    
     internals.shuffleArray(answers);
     console.log(answers);
     return answers;
-  }
+  };
 
   internals.checkAnswer = function (selectedAnswer, correctAnswer) {
     return selectedAnswer === correctAnswer;
   };
 
-
-  internals.renderQuizz = function (question) {
-      //var quizzCard = $("#app");
-      //if (quizzCard) {
-      //quizzCard.empty();
-    //}
-    internals.createCategory();
-    
-    let questionCreated = internals.createQuestion(question);
-    $("#question").append(questionCreated);
-    $("#questionNumber").append(`Question: ` + questionNumber + `/10`);
-
-    internals.renderAnswers(question);
-
-    internals.incrementIndex();
-    internals.incrementQuestion();
-    console.log("A PRÓXIMA PERGUNTA SERIA: " + question.results[indexRandom].question);
-  };
-
-  internals.incrementIndex = function(){
-     if (indexRandom === 9) {
-      indexRandom = 0;
-    }
-    indexRandom++;
-    console.log(indexRandom);
-  }
-
-  internals.incrementQuestion = function(){
-    if(questionNumber === 10){
-      questionNumber = 1;
-    }
-    questionNumber++;
-    console.log(questionNumber);
-  }
-
-  internals.renderAnswers = function (answers){
-    const shuffledAnswers = internals.createAnswers(answers);
-    const correctAnswer = answers.results[indexRandom].correct_answer;
-    
-
-     $(".choice-text").each(function (index){
-      $(this).text(shuffledAnswers[index]);
-      $(this).off("click");
-
-      $(this).on("click", function(){
-        const selectedAnswer = $(this).text();
-
-        const isCorrect = internals.checkAnswer(selectedAnswer, correctAnswer);
-        const correct = `<p>${correctAnswer} is the correct answer!</p>`
-       
-        var emptyResponse = $("#response");
-
-        if(isCorrect){
-          console.log(correctAnswer + " is Correct");
-          $(emptyResponse).empty();
-          $("#response").append(correct);
-          $(this).css("background-color", "#97E67C");
-          $(this).css("color", "white")
-          $(this).css("border", "1px solid #97E67C")
-          internals.createNexButton();
-        } else {
-          console.log("Wrong");
-          $(emptyResponse).empty();
-          $("#nextbutton").empty();
-          $("#response").append("Wrong answer. Try another!")
-          $(this).css("background-color", "#D85846");
-          $(this).css("color", "white")
-          $(this).css("border", "1px solid #D85846")
-        }
-      })
-    });
-  };
-
-  internals.createNexButton= function(){
-    var button = `<button id ="button1"> Next Question </button>`;
-    $("#nextbutton").append(button);;
-    internals.nextQuestionButton();
-  }
-
-  internals.nextQuestionButton = function(){
-    console.log("HELOOOOOO")
-    let button1 = $("#button1");
-    $(button1).on("click", function(){
-      internals.renderQuizz(question);  
-    });
-  };
+  internals.renderAnswers = function (answers) {
+    console.log(answers);
+    var shuffledAnswers = internals.createAnswers(answers);
+    var correctAnswer = answers.results[indexRandom].correct_answer;
   
-  internals.createCategory= function(){
-    var insert = "Category is: Animals";
+    var choicesContainer = $(".choice-container");
+    choicesContainer.empty();
+  
+    shuffledAnswers.forEach(function (choice, index) {
+      var button = $("<button>")
+        .text(choice)
+        .addClass("choice-text")
+        .on("click", function () {
+          const selectedAnswer = $(this).text();
+          const isCorrect = internals.checkAnswer(selectedAnswer, correctAnswer);
+          const correct = `<p>${correctAnswer} is the correct answer!</p>`;
+  
+          var emptyResponse = $("#response");
+  
+          if (isCorrect) {
+            console.log(correctAnswer + " is Correct");
+            internals.createNextButton(answers);
+            $(emptyResponse).empty().append(correct);
+            $(this).css("background-color", "#97E67C");
+            $(this).css("color", "white");
+            $(this).css("border", "1px solid #97E67C");
+
+          } else {
+
+            console.log("Wrong");
+            $(emptyResponse).empty();
+            $("#nextbutton").empty();
+            $("#response").empty().append("Wrong answer. Try another!");
+            $(this).css("background-color", "#D85846");
+            $(this).css("color", "white");
+            $(this).css("border", "1px solid #D85846");
+          }
+        });
+  
+      choicesContainer.append(button);
+    });
+  };
+
+  internals.resetButtonStyles = function () {
+    $(".choice-text").each(function () {
+      $(this).css({
+        "background-color": "",  
+        "color": "",             
+        "border": ""             
+      });
+    });
+  };
+
+  internals.incrementIndex = function () {
+    if (indexRandom === 9) {
+      indexRandom = 0;
+    } else {
+      indexRandom++;
+    }
+    console.log(indexRandom);
+  };
+
+  internals.incrementQuestion = function () {
+    if (questionNumber === 10) {
+      questionNumber = 1;
+    } else {
+      questionNumber++;
+    }
+    console.log(questionNumber);
+  };
+
+  internals.createNextButton = function (question) {
+    $("#nextbutton").empty();
+    var button = `<button id ="button1"> Next Question </button>`;
+    $("#nextbutton").append(button);
+    internals.incrementIndex();
+    internals.nextQuestionButton(question);
+  };
+
+  internals.nextQuestionButton = function (question) {
+    var button1 = $("#button1");
+    var emptyResponse = $("#response");
+
+    $(button1).off("click");
+    $(button1).on("click", function () {
+
+      internals.renderQuizz(question);
+      $(button1).remove();
+      emptyResponse.empty();
+      internals.resetButtonStyles();
+
+    });
+
+  };
+
+  internals.createCategory = function (question) {
+    var insert = "Category is: " + question.results[indexRandom].category;
     $("#category").empty().append(insert);
+  };
+
+
+  internals.createStartMenu = function(){
+
+    var menu = `<div id="trivia"> <span>T</span>rivia <span>Q</span>uizz </div>`;
+    var startButton = `<div id=startButton><button id ="button2" style="--clr:#df6694"> Start the Quizz <i></i></button></div>`
+    $("#game").append(menu);
+    $("#game").append(startButton); 
   }
+
+  internals.renderStartButton = function (question) {
+    internals.createStartMenu();
+    var button2 = $("#button2");
+    var emptyTrivia = $("#game");
+
+    $(button2).off("click");
+    $(button2).on("click", function () {
+      $(button2).remove();
+      emptyTrivia.empty();
+      internals.renderQuizz(question);
+    });
+
+  };
 
   externals.render = function (question) {
-    internals.renderQuizz(question);
+    //internals.renderQuizz(question);
+    //internals.createStartMenu(question);
+    internals.renderStartButton(question);
   };
   return externals;
 });
